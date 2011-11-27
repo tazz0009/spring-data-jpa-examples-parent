cf269caa487eb77b43b4cd4d016f915504136eb9
Seung Hwan Choi <tazz0009@gmail.com>
2011-11-27 13:52:00 +0900

    first commit

---
+++ src/main/java/org/springframework/data/jpa/examples/spring_data_jpa_examples_parent/App.java
@@ -0,0 +1,13 @@
+package org.springframework.data.jpa.examples.spring_data_jpa_examples_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
